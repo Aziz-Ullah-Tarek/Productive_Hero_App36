e6a77e294fb132e253275fd484105053746871d9
AZIZ ULLAH TAREK <azizullaht2002@gmail.com>
2025-10-08 20:30:13 +0600

    ErrorPage added

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-
+import errorBg from "../assets/App-Error.png";
+import { Link } from "react-router";
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 Page Not Found</h1>
+    <div className="font-inter">
+      <div className="flex flex-col justify-center items-center h-screen gap-5">
+        <img src={errorBg} alt="" />
+        <h1 className="text-4xl font-bold">Oops, page not found!</h1>
+        <p>The page you are looking for is not available.</p>
+        <Link
+          to={"/"}
+          className=" btn btn-lg bg-linear-120 from-purple-600 to-blue-800 text-white
+            rounded-lg "
+        >
+          Back to the Home Page!
+        </Link>
+      </div>
     </div>
   );
 };
